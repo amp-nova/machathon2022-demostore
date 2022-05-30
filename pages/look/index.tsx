aa8a8c70c9368734b309468cd9e6c866d83bf6c5
Réza Kalfane <reza.kalfane@gmail.com>
2022-05-31 00:39:29 +0200

    Working facets

--- pages/look/index.tsx
+++ pages/look/index.tsx
@@ -2,7 +2,7 @@ import fetchStandardPageData from "@lib/page/fetchStandardPageData";
 import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
 import React, { useEffect } from "react";
 import { Layout } from "@components/core";
-import { Grid, Typography } from "@mui/material";
+import { Button, Grid, Typography } from "@mui/material";
 import LookCard from "@components/cms-modern/Look/LookCard";
 const ConstructorIOClient = require("@constructor-io/constructorio-client-javascript");
 
@@ -20,6 +20,11 @@ interface FacetsProps {
   options: Array<any>;
 }
 
+interface FilterProps {
+  brand?: string;
+  color?: string;
+}
+
 export default function LookMainPage(
   { content }
     : InferGetServerSidePropsType<typeof getServerSideProps>) {
@@ -30,6 +35,9 @@ export default function LookMainPage(
 
   const [brandFacet, setbrandFacet] = React.useState({} as FacetsProps);
   const [colorFacet, setcolorFacet] = React.useState({} as FacetsProps);
+  const [selectedBrand, setselectedBrand] = React.useState("" || null);
+  const [selectedColor, setselectedColor] = React.useState("" || null);
+
 
   useEffect(() => {
     console.log("USE EFFECT");
@@ -39,22 +47,31 @@ export default function LookMainPage(
       // sessionId: 1234,
       // clientId: "1234"
     });
-    
+
+    const filters = {} as FilterProps;
+    if (selectedBrand) {
+      filters.brand = selectedBrand;
+    }
+    if (selectedColor) {
+      filters.color = selectedColor;
+    }
+
     constructorClient.search.getSearchResults('https://amplience.com/look', {
       section: "Looks",
-      // filters: {
-      //   color: "pink"
-      // },
-      // filters: {
-      //   brand: "Ellos Collection"
-      // },
+      filters,
       resultsPerPage: 50
     }).then((data: any) => {
       console.log("DATA", data);
       setlooksList(data.response.results);
       setlooksNumber(data.response.total_num_results);
-      setbrandFacet(data.response.facets[0])
-      setcolorFacet(data.response.facets[1])
+      data.response.facets.forEach((item: any) => {
+        if (item.display_name === 'brand') {
+          setbrandFacet(item)
+        }
+        if (item.display_name === 'color') {
+          setcolorFacet(item)
+        }
+      })
     }).catch((e: any) => { console.log("ERROR", e) });
 
     constructorClient.recommendations.getRecommendations('looks_page', {
@@ -65,49 +82,54 @@ export default function LookMainPage(
       setviewedLooksList(data.response.results);
     }).catch((e: any) => { console.log("ERROR", e) });
 
-  },[])
+  }, [selectedBrand, selectedColor])
 
   return (
     <div className="af-main-content" style={{ paddingBottom: 60 }}>
       <Typography style={{ marginTop: 30, marginBottom: 20 }} variant="h2" component="h2">Shop Your Looks</Typography>
-      <Typography style={{ marginTop: 30, marginBottom: 20 }} variant="body1" component="p">{looksNumber * 3} looks found for you</Typography>
+      <Typography style={{ marginTop: 30, marginBottom: 20 }} variant="body1" component="p">{looksNumber} looks found for you</Typography>
 
       <Grid container>
         <Grid item md={2} xs={12}>
-      <Typography variant="h3" component="h3">{brandFacet.display_name}</Typography>
-      {
-        brandFacet?.options?.map((item: any, i: number) => { 
-          return <div key={i}>{`${item.display_name} (${item.count * 3})`}</div>
-        })
-      }
-      
-      <Typography variant="h3" component="h3" style={{paddingTop: 30}}>{colorFacet.display_name}</Typography>
-      {
-        colorFacet?.options?.map((item: any, i: number) => { 
-          return <div key={i}>{`${item.display_name} (${item.count * 3})`}</div>
-        })
-      }
-</Grid>
-<Grid item md={10} xs={12}>
-      <Grid container style={{display: "flex", justifyContent: "flex-start", flexWrap: "wrap", listStyle: "none", margin: 0, padding: 0 }}>
-        {
-          looksList.map((look: any, i: number) => { return <LookCard key={i} {...look.data} deliveryKey={look.data._meta.deliveryKey} deliveryId={look.data._meta.deliveryId}/> } )
-        }
-        {
-          looksList.map((look: any, i: number) => { return <LookCard key={i} {...look.data} deliveryKey={look.data._meta.deliveryKey} deliveryId={look.data._meta.deliveryId}/> } )
-        }
-        {
-          looksList.map((look: any, i: number) => { return <LookCard key={i} {...look.data} deliveryKey={look.data._meta.deliveryKey} deliveryId={look.data._meta.deliveryId}/> } )
-        }
-      </Grid>
 
-      <Typography style={{ marginTop: 30, marginBottom: 20 }} variant="h2" component="h2">Recently Viewed Looks</Typography>
-      <Grid container style={{display: "flex", justifyContent: "flex-start", flexWrap: "wrap", listStyle: "none", margin: 0, padding: 0 }}>
-        {
-          viewedLooksList.map((look: any, i: number) => { return <LookCard key={i} {...look.data} deliveryKey={look.data._meta.deliveryKey} deliveryId={look.data._meta.deliveryId}/> } )
-        }
-      </Grid>
-      </Grid>
+          {
+            brandFacet &&
+            <>
+              <div>Selection: {selectedBrand} <Button style={{ backgroundColor: "#fff" }} onClick={(e: any) => { setselectedBrand(null) }}>x</Button></div>
+              <Typography variant="h3" component="h3">{brandFacet?.display_name}</Typography>
+              {
+                brandFacet?.options?.map((item: any, i: number) => {
+                  return <div key={i}><Button style={{ backgroundColor: "#fff" }} onClick={(e: any) => { setselectedBrand(item.display_name) }}>{`${item.display_name} (${item.count})`}</Button></div>
+                })
+              }
+            </>
+          }
+          {
+            colorFacet &&
+            <><div style={{ paddingTop: 30 }}>Selection: {selectedColor} <Button style={{ backgroundColor: "#fff" }} onClick={(e: any) => { setselectedColor(null) }}>x</Button></div>
+              <Typography variant="h3" component="h3">{colorFacet?.display_name}</Typography>
+              {
+                colorFacet?.options?.map((item: any, i: number) => {
+                  return <div key={i}><Button style={{ backgroundColor: "#fff" }} onClick={(e: any) => { setselectedColor(item.display_name) }}>{`${item.display_name} (${item.count})`}</Button></div>
+                })
+              }
+            </>
+          }
+        </Grid>
+        <Grid item md={10} xs={12}>
+          <Grid container style={{ display: "flex", justifyContent: "flex-start", flexWrap: "wrap", listStyle: "none", margin: 0, padding: 0 }}>
+            {
+              looksList.map((look: any, i: number) => { return <LookCard key={i} {...look.data} deliveryKey={look.data._meta.deliveryKey} deliveryId={look.data._meta.deliveryId} /> })
+            }
+          </Grid>
+
+          <Typography style={{ marginTop: 30, marginBottom: 20 }} variant="h2" component="h2">Recently Viewed Looks</Typography>
+          <Grid container style={{ display: "flex", justifyContent: "flex-start", flexWrap: "wrap", listStyle: "none", margin: 0, padding: 0 }}>
+            {
+              viewedLooksList.map((look: any, i: number) => { return <LookCard key={i} {...look.data} deliveryKey={look.data._meta.deliveryKey} deliveryId={look.data._meta.deliveryId} /> })
+            }
+          </Grid>
+        </Grid>
       </Grid>
     </div>
   );
